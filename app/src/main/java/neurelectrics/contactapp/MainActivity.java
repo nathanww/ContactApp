ec170f45b4e9eec52e8500c1fc90f69976033f5e
Nathan Whitmore <nathanww@gmail.com>
2020-03-29 14:24:30 -0500

    App graph of contacts over the past day

--- app/src/main/java/neurelectrics/contactapp/MainActivity.java
+++ app/src/main/java/neurelectrics/contactapp/MainActivity.java
@@ -22,6 +22,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -31,6 +32,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -46,6 +48,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -57,7 +61,29 @@ public class MainActivity extends AppCompatActivity {
     //hashMap that stores bt device info index by address
     HashMap<String, ScanResult> results = new HashMap<String, ScanResult>();
 
+    String generateChartString() { //format the hourly exposure data into a format that can be sent to quickchart.
+        String BASE_REQUEST = "https://quickchart.io/chart?c={type:%27line%27,%20options:%20{legend:%20{display:%20false}},data:{labels:[%2712%20AM%27,%271%20AM%27,%272%20AM%27,%273%20AM%27,%274%20AM%27,%275%20AM%27,%276%20AM%27,%277%20AM%27,%278%20AM%27,%279%20AM%27,%2710%20AM%27,%2711%20AM%27,%2712%20PM%27,%20%271%20PM%27,%272%20PM%27,%273%20PM%27,%274%20PM%27,%275%20PM%27,%276%20PM%27,%277%20PM%27,%278%20PM%27,%279%20PM%27,%2710%20PM%27,%2711%20PM%27],%20datasets:[{label:%27%27,%20data:%20[#CHARTDATA#],%20fill:false,borderColor:%27blue%27}]}}";
+        String hourlyData = "";
+        SimpleDateFormat todayFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        String todayKey = todayFormat.format(Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault()).getTime());
+        final SharedPreferences prefs = getSharedPreferences("com", MODE_PRIVATE);
 
+        //go through each hour of the day and append teh number of contacts to hourlydata.
+        boolean hasValidData = false; //if we have some valid data but not readings for a time preceding this, the time preceding will be set to zero to make the chart look nice. However data that has not been recorded yet is not plotted.
+        for (int time = 0; time <= 23; time++) {
+            int contactNum = prefs.getInt(time + "-" + todayKey, -1);
+            Log.i("contacthour", time + ", " + contactNum);
+            if (contactNum > -1) { //we actually have data for this slot
+                hourlyData = hourlyData + contactNum + ",";
+                hasValidData = true;
+            } else if (!hasValidData || true) {
+                hourlyData = hourlyData + "0,";
+            }
+        }
+        //hourlyData=hourlyData+"END".replace(",END",""); //ret rid of trailing ,
+        BASE_REQUEST = BASE_REQUEST.replace("#CHARTDATA#", hourlyData); //plug the data into the URL
+        return BASE_REQUEST;
+    }
     boolean isWearable(BluetoothDevice bd) {
         int btClass = bd.getBluetoothClass().getDeviceClass();
         int majorClass = bd.getBluetoothClass().getMajorDeviceClass();
@@ -114,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 // first update the total number of contacts today
                 SimpleDateFormat todayFormat = new SimpleDateFormat("dd-MMM-yyyy");
-                String todayKey = todayFormat.format(Calendar.getInstance().getTime());
+                String todayKey = todayFormat.format(Calendar.getInstance(TimeZone.getDefault(), Locale.getDefault()).getTime());
                 contactsToday.setText("Exposure today: " + prefs.getInt(todayKey, 0));
 
                 String dispResult = "";
@@ -133,6 +159,17 @@ public class MainActivity extends AppCompatActivity {
         };
 // start
         handler.postDelayed(updateLoop, 5000);
+
+        //set up the exposure chart using quickchart.io to make the chart and Webview to display it
+        WebView chartView = (WebView) findViewById(R.id.chartView);
+        chartView.setInitialScale(30);
+        chartView.setBackgroundColor(Color.WHITE);
+        chartView.getSettings().setLoadWithOverviewMode(true); //set scaling to automatically fit the image returned by server
+        chartView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        chartView.setScrollbarFadingEnabled(false);
+        chartView.getSettings().setUseWideViewPort(true);
+
+        chartView.loadUrl(generateChartString());
     }
 
 
