b3291fe2f435ba40a4304c91d0a18a54818c91f9
Nathan Whitmore <nathanww@gmail.com>
2020-04-07 10:32:18 -0500

    Fix bug with autostart on boot to make it compatible with Android background execution limits

--- app/src/main/java/neurelectrics/contactapp/autostart.java
+++ app/src/main/java/neurelectrics/contactapp/autostart.java
@@ -3,16 +3,23 @@ package neurelectrics.contactapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class autostart extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("trackickingService", "attempting to start on boot");
+        Log.i("trackingService", "attempting to start on boot");
         Intent startserv = new Intent(context, MyForeGroundService.class);
         startserv.setAction(MyForeGroundService.ACTION_START_FOREGROUND_SERVICE);
-        context.startService(startserv);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(startserv);
+        } else {
+            context.startService(startserv);
+        }
 
     }
 }
